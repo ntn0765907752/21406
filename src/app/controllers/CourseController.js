49b89b499fcb4a703eb7d8f7875e56c6262220de
ntn0765907752 <nghiant21406@st.uel.edu.vn>
2023-06-01 20:59:55 +0700

    add btn checkbox full

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -5,12 +5,10 @@ class CourseController {
     //[GET] /courses/:slug
     show(req, res, next) {
         Course.findOne({ slug: req.params.slug }).lean()
-        .then(course =>{
-            res.render('../courses/show',{course});
-            
-
-        })
-        .catch(next);
+            .then(course =>{
+                res.render('../courses/show',{course});
+            })
+            .catch(next);
         
     }
     //[GET] /courses/create
@@ -28,7 +26,6 @@ class CourseController {
             .catch(error=>{});
     }
     //[GET] /courses/:id/edit
-    //[GET] /courses/:id/edit
     edit(req, res, next) {
         Course.findById(req.params.id).lean()
         .then(course =>{
@@ -37,7 +34,6 @@ class CourseController {
         
         .catch(next);
     }
-    //[PUT] /courses/:id
     //[PUT] /courses/:id
     update(req, res, next) {
         Course.updateOne({_id: req.params.id }, req.body).lean()
